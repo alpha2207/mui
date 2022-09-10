830ee5d394b82b0b8c2a3e91bee3d6b338d037ca
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-09-10 17:48:28 +0530

    Used basic component and icons.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Button } from "@mui/material";
+import { Delete,Add, Send } from "@mui/icons-material";
+
 function App() {
+
   return (
     <div>
-      Hello
+      <Button startIcon={<Delete />} variant="outlined" color="error" sx={{m:2}}>Text</Button>
+      <Button endIcon={<Send />} variant="contained" sx={{m:2}}>Send</Button>
+      <Button variant="outlined" sx={{m:2}}>Outlined</Button>
     </div>
   );
 }
