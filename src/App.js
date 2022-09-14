68e8786a27a9b4560d2bafa1ee80af78f11c87a9
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-09-14 09:56:20 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import { Stack } from "@mui/system";
 import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
 import Rightbar from "./components/Rightbar";
 import Sidebar from "./components/Sidebar";
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
    <Box>
+    <Navbar/>
     <Stack direction='row' justifyContent='space-between' spacing={2}>
       <Sidebar/>
       <Feed/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { AppBar,styled, Toolbar, Typography } from '@mui/material'
+import React from 'react';
+import PetsIcon from '@mui/icons-material/Pets';
+
+const StyledToolbar=styled(Toolbar)({
+  display:'flex',
+  justifyContent:'space-between'
+})
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <AppBar position='sticky'>
+      <StyledToolbar>
+        <PetsIcon sx={{display:{xs:'block',sm:'none'}}}/>
+        <Typography sx={{display:{xs:'none',sm:'block'}}} variant='h6'>alpha_dev</Typography>
+
+        <SearchBar>
+          
+        </SearchBar>
+      </StyledToolbar>
+    </AppBar>
   )
 }
