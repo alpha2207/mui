ddc33c75cd72ec713c5b99bfeb5df15e4f7cdfe6
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-09-15 09:54:35 +0530

    Added navbar component and body styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,62 @@
-import { AppBar,styled, Toolbar, Typography } from '@mui/material'
+import { AppBar, styled, Toolbar, Typography, InputBase, Box, Badge, Avatar } from '@mui/material'
 import React from 'react';
+import MailIcon from '@mui/icons-material/Mail';
 import PetsIcon from '@mui/icons-material/Pets';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+// import { Theme } from '@mui/material';
 
-const StyledToolbar=styled(Toolbar)({
-  display:'flex',
-  justifyContent:'space-between'
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between'
+})
+
+const Search = styled('div')({
+  display: 'flex',
+  width: '40%',
+  background: 'white',
+  padding: '0 10px',
+  borderRadius: '4px'
+})
+
+const Icons = styled(Box)({
+  display: 'flex',
+  gap: "20px",
+  alignItems: 'center'
+})
+
+const UserBox = styled(Box)({
+  display: 'flex',
+  gap: "10px",
+  alignItems: 'center'
 })
 
 export default function Navbar() {
   return (
     <AppBar position='sticky'>
       <StyledToolbar>
-        <PetsIcon sx={{display:{xs:'block',sm:'none'}}}/>
-        <Typography sx={{display:{xs:'none',sm:'block'}}} variant='h6'>alpha_dev</Typography>
+        <PetsIcon sx={{ display: { xs: 'block', sm: 'none' } }} />
+        <Typography sx={{ display: { xs: 'none', sm: 'block' } }} variant='h6'>alpha_dev</Typography>
 
-        <SearchBar>
-          
-        </SearchBar>
+        <Search>
+          <InputBase placeholder='Search...' />
+        </Search>
+
+        <Icons sx={{display:{xs:'none',sm:'flex'}}}>
+          <Badge badgeContent={4} color="error">
+            <MailIcon color="white" />
+          </Badge>
+
+          <Badge badgeContent={4} color="error">
+            <NotificationsActiveIcon color="white" />
+          </Badge>
+
+          <Avatar sx={{ width: 30, height: 30 }} alt="Remy Sharp" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
+        </Icons>
+
+        <UserBox sx={{display:{xs:'block',sm:'none'}}}>
+          <Avatar sx={{ width: 30, height: 30 }} alt="Remy Sharp" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
+        </UserBox>
+
       </StyledToolbar>
     </AppBar>
   )
