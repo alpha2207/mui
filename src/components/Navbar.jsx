672c2479b0bd6b06c4dc8d2c7b6969f987ea542b
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-09-15 10:46:02 +0530

    Designed Sidebar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { AppBar, styled, Toolbar, Typography, InputBase, Box, Badge, Avatar } from '@mui/material'
-import React from 'react';
+import { AppBar, styled, Toolbar, Typography, InputBase, Box, Badge, Avatar, Menu, MenuItem } from '@mui/material'
+import React, { useState } from 'react';
 import MailIcon from '@mui/icons-material/Mail';
 import PetsIcon from '@mui/icons-material/Pets';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
@@ -31,6 +31,8 @@ const UserBox = styled(Box)({
 })
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <AppBar position='sticky'>
       <StyledToolbar>
@@ -41,7 +43,7 @@ export default function Navbar() {
           <InputBase placeholder='Search...' />
         </Search>
 
-        <Icons sx={{display:{xs:'none',sm:'flex'}}}>
+        <Icons sx={{ display: { xs: 'none', sm: 'flex' } }}>
           <Badge badgeContent={4} color="error">
             <MailIcon color="white" />
           </Badge>
@@ -53,11 +55,31 @@ export default function Navbar() {
           <Avatar sx={{ width: 30, height: 30 }} alt="Remy Sharp" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
         </Icons>
 
-        <UserBox sx={{display:{xs:'block',sm:'none'}}}>
+        <UserBox sx={{ display: { xs: 'block', sm: 'none' } }}>
           <Avatar sx={{ width: 30, height: 30 }} alt="Remy Sharp" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
         </UserBox>
 
+
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+
+      
     </AppBar>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,71 @@
 import React from 'react'
 import Box from '@mui/material/Box'
+import { List, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from '@mui/material'
+import { Home } from '@mui/icons-material'
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import GroupIcon from '@mui/icons-material/Group';
 
 export default function Sidebar() {
   return (
-    <Box p={2} flex={1} bgcolor="lightcoral" sx={{display:{xs:'none',sm:'block'}}}>Sidebar</Box>
+    <Box p={2} flex={1} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <List>
+        <ListItemButton component='a' href='#home'>
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <ListItemText primary="Homepage" />
+        </ListItemButton>
+
+        <ListItemButton component='a' href='#home'>
+          <ListItemIcon>
+            <AssignmentIcon />
+          </ListItemIcon>
+          <ListItemText primary="Pages" />
+        </ListItemButton>
+
+        <ListItemButton component='a' href='#home'>
+          <ListItemIcon>
+            <GroupIcon />
+          </ListItemIcon>
+          <ListItemText primary="Groups" />
+        </ListItemButton>
+
+        <ListItemButton component='a' href='#home'>
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <ListItemText primary="Homepage" />
+        </ListItemButton>
+
+        <ListItemButton component='a' href='#home'>
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <ListItemText primary="Homepage" />
+        </ListItemButton>
+
+        <ListItemButton component='a' href='#home'>
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <ListItemText primary="Homepage" />
+        </ListItemButton>
+
+        <ListItemButton component='a' href='#home'>
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <ListItemText primary="Homepage" />
+        </ListItemButton>
+
+        <ListItemButton component='a' href='#home'>
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <ListItemText primary="Homepage" />
+        </ListItemButton>
+
+      </List>
+    </Box>
   )
 }
